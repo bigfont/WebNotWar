c4111e5f8a431ed460099f846c4f90ebee12cd29
Shaun Luttin <admin@shaunluttin.com>
2016-02-19 14:29:31 -0800

    Remove additional cruft.

--- AspNet5RC1/Startup.cs
+++ AspNet5RC1/Startup.cs
@@ -1,18 +1,12 @@
 ﻿using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace AspNet5RC1
 {
     public class Startup
     {
         public Startup(IHostingEnvironment env) { }
-
-        public IConfigurationRoot Configuration { get; set; }
-
-        public void ConfigureServices(IServiceCollection services) { }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
